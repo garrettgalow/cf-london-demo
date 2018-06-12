b90fb8f5e95706242bc937e4771eb1ab0765131e
Garrett Galow <neogeo777@gmail.com>
2018-06-12 19:52:39 +0100

    add local stuff

--- cloudflare.tf
+++ cloudflare.tf
@@ -63,6 +63,36 @@ resource "cloudflare_load_balancer_monitor" "aks_monitor" {
    description = "AKS Monitor"
 }
 
+resource "cloudflare_load_balancer_monitor" "local_monitor" {
+   method = "GET"
+   expected_body = "OK"
+   expected_codes = "2xx"
+   path = "/api/v4/system/ping"
+   timeout = 5
+   interval = 30
+   retries = 1
+   description = "Local Monitor"
+}
+
+resource "cloudflare_load_balancer_pool" "local_pool" {
+   name = "local-pool"
+   origins {
+      name = "argo-tunnel-1"
+      address = ""
+      enabled = true
+   }
+   origins {
+      name = "argo-tunnel-2"
+      address = ""
+      enabled = true
+   }
+   description = "Local Origin"
+   enabled = true
+   minimum_origins = 1
+   notification_email = "gg@cloudflare.com"
+   monitor = "${cloudflare_load_balancer_monitor.local_monitor.id}"
+}
+
 resource "cloudflare_load_balancer_pool" "aks_pool" {
    name = "aks-pool"
    origins {
@@ -71,7 +101,7 @@ resource "cloudflare_load_balancer_pool" "aks_pool" {
       enabled = true
    }
    description = "Azure Kubernetes Origin"
-   enabled = true
+   enabled = false
    minimum_origins = 1
    notification_email = "gg@cloudflare.com"
    monitor = "${cloudflare_load_balancer_monitor.aks_monitor.id}"
@@ -85,7 +115,7 @@ resource "cloudflare_load_balancer_pool" "gke_pool" {
       enabled = true
    }
    description = "Google Kubernetes Origin"
-   enabled = true
+   enabled = false
    minimum_origins = 1
    notification_email = "gg@cloudflare.com"
    monitor = "${cloudflare_load_balancer_monitor.gke_monitor.id}"
@@ -93,9 +123,9 @@ resource "cloudflare_load_balancer_pool" "gke_pool" {
 
 resource "cloudflare_load_balancer" "chat_demo" {
    zone = "cloudflare.london"
-   name = "chat.cloudflare.london"
+   name = "cloudflare.london"
    fallback_pool_id = "${cloudflare_load_balancer_pool.aks_pool.id}"
-   default_pool_ids = ["${cloudflare_load_balancer_pool.aks_pool.id}","${cloudflare_load_balancer_pool.gke_pool.id}"]
+   default_pool_ids = ["${cloudflare_load_balancer_pool.aks_pool.id}","${cloudflare_load_balancer_pool.gke_pool.id}","${cloudflare_load_balancer_pool.local_pool.id}"]
    description = "Load Balancer for MatterMost Chat"
    proxied = true
    region_pools {
