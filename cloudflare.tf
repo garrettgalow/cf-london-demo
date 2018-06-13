dd97bcef0d9bd3440ff2f18d8f088313209e68c4
Garrett Galow <neogeo777@gmail.com>
2018-06-13 10:20:29 +0100

    change fallback pool to gke

--- cloudflare.tf
+++ cloudflare.tf
@@ -91,7 +91,7 @@ resource "cloudflare_load_balancer" "chat_demo" {
    zone = "cloudflare.london"
    name = "prod.cloudflare.london"
    default_pool_ids = ["${cloudflare_load_balancer_pool.aks_pool.id}","${cloudflare_load_balancer_pool.gke_pool.id}"]
-   fallback_pool_id = "${cloudflare_load_balancer_pool.aks_pool.id}"
+   fallback_pool_id = "${cloudflare_load_balancer_pool.gke_pool.id}"
    description = "Load Balancer for MatterMost Chat"
    proxied = true
    region_pools {
